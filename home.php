0c15ec0455bfe08ff5bcefe20a11eaa5c3cf2f23
Al Nemec <al.nemec@gmail.com>
2014-04-25 23:15:25 -0500

    Added new graphic assets and styles

--- home.php
+++ home.php
@@ -24,13 +24,13 @@
 			$attachment_id = get_field('screenshot'); $size = "medium";
 			$image = wp_get_attachment_image_src($attachment_id, $size);
 
-
-			echo "<img class='screenshot' src='".$image[0]."' alt=' ' >";
+			echo "<div class='dateCircle'>Apr<span>14</span></div>";
+			echo "<div class='screenshotContainer'><img class='screenshot' src='".$image[0]."' alt=' ' ></div>";
 
 	endif; ?>
 
 		<div class="entry-content">
-			
+
 		<h2><?php the_title(); ?></h2>
 
 
@@ -50,12 +50,14 @@
 
 			//echo "Count: " . count($commits) . "\n";
 			$hash = $commits[0]->getSha();
+			
 			//$hash = $singlecommit->getSha();
-			echo "<a href='http://github.com/".$owner."/".$repo."/commit/".$hash."'>Latest Commit: ".$hash."</a>";
+			echo "<div class='hashLabel'>Latest Commit</div><a href='http://github.com/".$owner."/".$repo."/commit/".$hash."' class='hashLink'>".$hash."<div class='hashFade'></div></a><div class='hashArrow'><svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 109.6 115.1' enable-background='new 0 0 109.6 115.1' xml:space='preserve'><polygon fill='#AE3424' points='0,0 0,115.1 109.6,0 '/></svg></div>";
 
 			?>
-
-		<?php the_content(); ?>
+			<div class="description">
+					<?php the_content(); ?>
+			</div>
 		</div>
 	</div>
 	<?php endwhile; wp_reset_query(); ?>
