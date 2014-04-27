76ab97f3b00649a23591383b1577170a9c886c7f
Al Nemec <al.nemec@gmail.com>
2014-04-26 21:33:35 -0500

    Updated admin bar availability and login page styles.

--- functions.php
+++ functions.php
@@ -8,7 +8,33 @@ just edit things like thumbnail sizes, header images,
 sidebars, comments, ect.
 */
 
+// Disable Admin Bar for everyone
+if (!function_exists('df_disable_admin_bar')) {
 
+    function df_disable_admin_bar() {
+
+        // for the admin page
+        remove_action('admin_footer', 'wp_admin_bar_render', 1000);
+        // for the front-end
+        remove_action('wp_footer', 'wp_admin_bar_render', 1000);
+
+        // css override for the admin page
+        function remove_admin_bar_style_backend() {
+            echo '<style>body.admin-bar #wpcontent, body.admin-bar #adminmenu { padding-top: 0px !important; }</style>';
+        }
+        add_filter('admin_head','remove_admin_bar_style_backend');
+
+        // css override for the frontend
+        function remove_admin_bar_style_frontend() {
+            echo '<style type="text/css" media="screen">
+            html { margin-top: 0px !important; }
+            * html body { margin-top: 0px !important; }
+            </style>';
+        }
+        add_filter('wp_head','remove_admin_bar_style_frontend', 99);
+    }
+}
+add_action('init','df_disable_admin_bar');
 
 // LOAD BONES CORE (if you remove this, the theme will break)
 require_once( 'library/bones.php' );
